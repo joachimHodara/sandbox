dbc8314c4a5714430d10a3330c98091071555d06
Joachim Hodara <joachim.hodara@3ds.com>
2019-04-30 09:07:03 -0400

    great new feature

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main()
 {
 	int i = 42;
+	int j = 13;
 
     cout << "The answer is " << i << endl;
     cout << "hello world" << endl;
+    cout << j << " is a bad answer" << endl;
 }
