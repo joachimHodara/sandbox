50c55f33681f26cdea39226c42151672c0bc3104
Joachim Hodara <joachim.hodara@3ds.com>
2019-04-30 09:09:53 -0400

    adding new function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void cool_function()
+{
+	cout << "hey there!" << endl;
+	cout << "Sweet function" << endl;
+}
+
 int main()
 {
 	int i = 42;
 
     cout << "The answer is " << i << endl;
     cout << "hello world" << endl;
+
+    cool_function();
 }
